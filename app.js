f17b10e877d6d1199330c8b7a91831660b2ed566
Swapnil Wadhankar <swapnilwadhankar12@gmail.com>
2020-03-11 18:28:59 +0530

    Item-list added to the UI

--- app.js
+++ app.js
@@ -1,44 +1,75 @@
-//local storage controller
+// Storage Controller
 
-//Item controller
+// Item Controller
 const ItemCtrl = (function() {
-  // Constructor
-  const Item = function(id, name, colories) {
+  // Item Constructor
+  const Item = function(id, name, calories) {
     this.id = id;
     this.name = name;
-    this.colories = colories;
+    this.calories = calories;
   };
 
-  //Data structure
+  // Data Structure / State
   const data = {
     items: [
-      { id: 0, name: "Steak-Dinner", colories: 1200 },
-      { id: 0, name: "Chicken-biryani", colories: 1300 },
-      { id: 0, name: "Eggs", colories: 400 }
+      { id: 0, name: "Steak Dinner", calories: 1200 },
+      { id: 1, name: "Cookie", calories: 400 },
+      { id: 2, name: "Eggs", calories: 300 }
     ],
     currentItem: null,
     totalCalories: 0
   };
 
-  //Public methods
+  // Public methods
   return {
+    getItems: function() {
+      return data.items;
+    },
     logData: function() {
       return data;
     }
   };
 })();
 
-//UI controller
-const UICtrl = (function() {})();
+// UI Controller
+const UICtrl = (function() {
+  const UISelectors = {
+    itemList: "#item-list"
+  };
 
-//App controller
+  // Public methods
+  return {
+    populateItemList: function(items) {
+      let html = "";
 
-const APP = (function(ItemCtrl, UICtrl) {
+      items.forEach(function(item) {
+        html += `<li class="collection-item" id="item-${item.id}">
+        <strong>${item.name}: </strong> <em>${item.calories} Calories</em>
+        <a href="#" class="secondary-content">
+          <i class="edit-item fa fa-pencil"></i>
+        </a>
+      </li>`;
+      });
+
+      // Insert list items
+      document.querySelector(UISelectors.itemList).innerHTML = html;
+    }
+  };
+})();
+
+// App Controller
+const App = (function(ItemCtrl, UICtrl) {
+  // Public methods
   return {
     init: function() {
-      console.log("App intializing .....");
+      // Fetch items from data structure
+      const items = ItemCtrl.getItems();
+
+      // Populate list with items
+      UICtrl.populateItemList(items);
     }
   };
 })(ItemCtrl, UICtrl);
 
-APP.init();
+// Initialize App
+App.init();
